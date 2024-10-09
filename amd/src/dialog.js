9b56f16ae126d38619358bc4e038e8aaed128236
Tobias Garske <tobias.garske@isb.bayern.de>
2024-10-09 08:35:42 +0000

    MBS-9409: Respect current language in replies

--- amd/src/dialog.js
+++ amd/src/dialog.js
@@ -11,6 +11,7 @@ import {renderUserQuota} from 'local_ai_manager/userquota';
 import {getAiConfig} from 'local_ai_manager/config';
 import LocalStorage from 'core/localstorage';
 import {escapeHTML, hash} from './helper';
+import Config from 'core/config';
 
 // Declare variables.
 const VIEW_CHATWINDOW = 'block_ai_chat_chatwindow';
@@ -295,8 +296,9 @@ const enterQuestion = async(question) => {
 
     // For first message, add a system message.
     if (conversation.messages.length === 0) {
+        const LangNames = new Intl.DisplayNames('en', {type: 'language'});
         conversation.messages.push({
-            'message': 'Answer in german',
+            'message': 'Answer in ' + LangNames.of(Config.language),
             'sender': 'system',
         });
     }
